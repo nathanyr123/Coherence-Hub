0fecea54031cf85823aa299014306ed454b26310
nathanyr123 <kttlor@gmail.com>
2025-07-22 14:17:35 -0600

    Update CoherenceHub.jsx

--- CoherenceHub.jsx
+++ CoherenceHub.jsx
@@ -1,4 +1,4 @@
-
+import { ChristosActivationModule } from './components/ChristosProtocol';
 import React, { useState, useEffect } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
